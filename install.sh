c409a5d9c6e96bffdba032928520725a85317668
Jonathan Lima <greenboxal@gmail.com>
2016-02-21 16:14:30 -0300

    Add automatic plugin installation

--- install.sh
+++ install.sh
@@ -5,6 +5,17 @@ set -e
 # Make sure tmp dir exists
 mkdir -p tmp
 
+# Make sure we have all submodules
+git submodule update --init --recursive
+
+# Make sure Vundle is at head
+cd bundle/Vundle.vim
+git checkout master
+cd ../..
+
+# Install plugins
+vim +PluginInstall +qall
+
 # Install tern for vim
 cd bundle/tern_for_vim
 npm install
