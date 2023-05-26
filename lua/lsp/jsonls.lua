b5481adcfb5bcf7ae1d4bb0930f5a0f48f1772f7
Alexandre de Souza <alexandre@aledsz.com.br>
2023-05-26 15:04:29 -0300

    add jsonls config

---
+++ lua/lsp/jsonls.lua
@@ -0,0 +1,8 @@
+-- JSON
+require("lspconfig").jsonls.setup {
+	cmd = { "vscode-json-language-server", "--stdio" },
+	filetypes = { "json", "jsonc" },
+	init_options = {
+		provideFormatter = true
+	},
+}
