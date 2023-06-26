568fbc7e90264449ba9de4b95700b3d48402cf6d
Alexandre de Souza <alexandre@aledsz.com.br>
2023-06-26 17:39:28 -0300

    update lua ls config

--- lua/lsp/lua_ls.lua
+++ lua/lsp/lua_ls.lua
@@ -1,5 +1,18 @@
 -- Lua
+local util = require("lspconfig.util")
+
 require("lspconfig").lua_ls.setup({
+	filetype = { "lua" },
+	root_dir = util.root_pattern(
+		".luarc.json",
+		".luarc.jsonc",
+		".luacheckrc",
+		".stylua.toml",
+		"stylua.toml",
+		"selene.toml",
+		"selene.yml",
+		".git"
+	),
 	settings = {
 		Lua = {
 			runtime = {
