59a0c0953816eea8e135e6601fd3ddc9a0501af2
Alexandre de Souza <alexandre@aledsz.com.br>
2024-02-29 14:34:56 -0300

    lint

--- lua/theme/neo-tree.lua
+++ lua/theme/neo-tree.lua
@@ -1,22 +1,22 @@
 local colors = require("theme.colors")
 
 local hi_groups = {
-	NeoTreeDirectoryIcon = { fg = colors.purple },
-	NeoTreeDirectoryName = { fg = colors.purple, bold = true },
-	NeoTreeRootName = { fg = colors.purple, bold = true },
-	NeoTreeFileName = { fg = colors.bright_purple },
-	NeoTreeFileIcon = { fg = colors.bright_purple },
-	NeoTreeFileNameOpened = { fg = colors.green },
-	NeoTreeIndentMarker = { fg = colors.purple },
-	NeoTreeGitAdded = { fg = colors.green },
-	NeoTreeGitConflict = { fg = colors.red },
-	NeoTreeGitModified = { fg = colors.orange },
-	NeoTreeGitUntracked = { fg = colors.yellow },
-	NeoTreeNormal = { bg = colors.menu, fg = colors.fg },
-	NeoTreeNormalNC = { fg = colors.bright_purple, bg = colors.menu },
-	NeoTreeSymbolicLinkTarget = { fg = colors.cyan },
+  NeoTreeDirectoryIcon = { fg = colors.purple },
+  NeoTreeDirectoryName = { fg = colors.purple, bold = true },
+  NeoTreeRootName = { fg = colors.purple, bold = true },
+  NeoTreeFileName = { fg = colors.bright_purple },
+  NeoTreeFileIcon = { fg = colors.bright_purple },
+  NeoTreeFileNameOpened = { fg = colors.green },
+  NeoTreeIndentMarker = { fg = colors.purple },
+  NeoTreeGitAdded = { fg = colors.green },
+  NeoTreeGitConflict = { fg = colors.red },
+  NeoTreeGitModified = { fg = colors.orange },
+  NeoTreeGitUntracked = { fg = colors.yellow },
+  NeoTreeNormal = { bg = colors.menu, fg = colors.fg },
+  NeoTreeNormalNC = { fg = colors.bright_purple, bg = colors.menu },
+  NeoTreeSymbolicLinkTarget = { fg = colors.cyan },
 }
 
 for group, spec in pairs(hi_groups) do
-	vim.api.nvim_set_hl(0, group, spec)
+  vim.api.nvim_set_hl(0, group, spec)
 end
