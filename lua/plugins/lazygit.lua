f93f9e984239c0ee4b1146daf524be7d32afaf0a
Alexandre de Souza <alexandre@aledsz.com.br>
2025-10-17 12:37:36 -0300

    fix layzgit border chars

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -5,7 +5,7 @@ vim.g.lazygit_floating_window_winblend = 0
 vim.g.lazygit_floating_window_scaling_factor = 0.9
 
 -- Customize lazygit popup window corner characters
-vim.g.lazygit_floating_window_border_chars = { "╭", "╮", "╰", "╯" }
+vim.g.lazygit_floating_window_border_chars = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" }
 
 -- Fallback to 0 if neovim-remote is not installed
 vim.g.lazygit_use_neovim_remote = 1
