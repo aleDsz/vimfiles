6a416a32a1957f7bcc5d5aca10a11e37fbf49cff
Alexandre de Souza <alexandre@aledsz.com.br>
2025-12-11 19:18:32 -0300

    use vim api to format on save

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -4,3 +4,17 @@ vim.o.updatetime = 250
 vim.api.nvim_command [[
   autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})
 ]]
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('LspFormatting', { clear = true }),
+  callback = function(event)
+    if event.data.client_id == 0 then return end
+
+    vim.api.nvim_create_autocmd('BufWritePre', {
+      buffer = event.buf,
+      callback = function()
+        vim.lsp.buf.format({ async = true, timeout_ms = 5000 })
+      end,
+    })
+  end,
+})
