41ecb95f88f34c2987a4adae59b2700c49e7b4bf
Alexandre de Souza <alexandre@aledsz.com.br>
2025-12-12 00:26:34 -0300

    fix lua and luau lsp configs

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -1,32 +1,35 @@
-local util = require("lspconfig.util")
-
 ---@type vim.lsp.Config
 return {
-	filetype = { "lua" },
-	root_dir = util.root_pattern(
-    "default.project.json",
-		".luarc.json",
-		".luarc.jsonc",
-		".luacheckrc",
-		".stylua.toml",
+  cmd = { "lua-language-server" },
+  filetypes = { "lua" },
+  root_markers = {
+    ".luarc.json",
+    ".luarc.jsonc",
+    ".luacheckrc",
+    ".stylua.toml",
 		"stylua.toml",
 		"selene.toml",
-		"selene.yml"
-	),
-	settings = {
-		Lua = {
-			runtime = {
-				version = "LuaJIT",
-			},
-			diagnostics = {
-				globals = { "vim" },
+		"selene.yml",
+    ".git",
+  },
+  settings = {
+    Lua = {
+      runtime = {
+        version = "LuaJIT",
+      },
+      diagnostics = {
+        enable = true,
+				globals = { "hs", "vim", "it", "describe", "before_each", "after_each" },
+        disable =  { "lowercase-global" },
 			},
 			workspace = {
 				library = vim.api.nvim_get_runtime_file("lua", true),
+        maxPreload = 9999,
+        preloadFileSize = 9999,
 			},
 			telemetry = {
 				enable = false,
 			},
-		},
-	},
+    },
+  },
 }
--- lsp/luau_lsp.lua
+++ lsp/luau_lsp.lua
@@ -1,19 +1,30 @@
-local util = require("lspconfig.util")
-
 ---@type vim.lsp.Config
 return {
 	filetype = { "luau" },
-	root_dir = util.root_pattern(".luaurc.json"),
+  root_markers = {
+    ".luaurc.json",
+    ".luaurc.jsonc",
+    ".luaucheckrc",
+    ".stylua.toml",
+		"stylua.toml",
+		"selene.toml",
+		"selene.yml",
+    ".git",
+  },
 	settings = {
-		Lua = {
+		Luau = {
 			runtime = {
-				version = "Luau",
-			},
-			diagnostics = {
-				globals = { "vim" },
+        version = "Luau",
+      },
+      diagnostics = {
+        enable = true,
+				globals = { "hs", "vim", "it", "describe", "before_each", "after_each" },
+        disable =  { "lowercase-global" },
 			},
 			workspace = {
 				library = vim.api.nvim_get_runtime_file("luau", true),
+        maxPreload = 9999,
+        preloadFileSize = 9999,
 			},
 			telemetry = {
 				enable = false,
