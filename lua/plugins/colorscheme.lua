a79d6cdecd99ad57450601f4c7e1ce5c0115a054
Alexandre de Souza <alexandre@aledsz.com.br>
2026-01-01 18:01:16 -0300

    improve dracula settings

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,2 +1,12 @@
 ---@type lazy.types.LazyPluginBase
-return { "Dracula/vim", name = "dracula" }
+return {
+  "Dracula/vim",
+  name = "dracula",
+  config = function()
+    -- Sets the colorscheme for terminal sessions too.
+    vim.cmd("colorscheme dracula")
+
+    -- Sets the full support for dracula
+    vim.g.dracula_full_special_attrs_support = true
+  end,
+}
