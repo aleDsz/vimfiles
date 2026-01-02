7d4ba93a9c1628f511416280f17b60688e3fc7cb
Alexandre de Souza <alexandre@aledsz.com.br>
2026-01-02 19:40:06 -0300

    style quotes

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,21 +1,21 @@
 -- You will likely want to reduce updatetime which affects CursorHold
 -- note: this setting is global and should be set only once
 vim.o.updatetime = 250
-vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
   callback = function()
     vim.diagnostic.open_float(nil, { focus = false })
   end,
 })
 
 -- Start with transparency
-vim.api.nvim_create_autocmd({ 'BufEnter', 'VimEnter' }, {
+vim.api.nvim_create_autocmd({ "BufEnter", "VimEnter" }, {
   callback = function()
-    vim.api.nvim_set_hl(0, 'Normal', { bg = 'NONE' })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
   end,
 })
 
 -- Enable Treesitter highlighting on file open (only if parser exists)
-vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   callback = function()
     local buf = vim.api.nvim_get_current_buf()
     if not vim.treesitter.highlighter.active[buf] then
@@ -28,13 +28,13 @@ vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
   end,
 })
 
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('LspFormatting', { clear = true }),
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("LspFormatting", { clear = true }),
   callback = function(event)
     local colorizer = require "colorizer"
     colorizer.attach_to_buffer(0)
 
-    vim.api.nvim_create_autocmd('BufWritePre', {
+    vim.api.nvim_create_autocmd("BufWritePre", {
       buffer = event.buf,
       callback = function()
         vim.lsp.buf.format()
