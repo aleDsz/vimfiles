2f836ec61b03963f4a81679981cc320735fe250d
Alexandre de Souza <alexandre@aledsz.com.br>
2026-02-02 14:28:44 -0300

    update 99 with new opts

--- lua/plugins/99.lua
+++ lua/plugins/99.lua
@@ -2,7 +2,15 @@
 return {
 	"aleDsz/99",
 	opts = {
+		completion = {
+			custom_rules = {},
+			source = "cmp",
+		},
 		display_errors = true,
+		md_files = {
+			"AGENT.md",
+			"CLAUDE.md",
+		},
 		model = "ollama/qwen2.5-coder:14b",
 	},
 }
