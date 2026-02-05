239caf739e2ce25cc7cdf26014ab88a1613c5ffb
Alexandre de Souza <alexandre@aledsz.com.br>
2026-02-05 13:10:29 -0300

    remove lsp document symbol completion

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -7,7 +7,6 @@ return {
 			"hrsh7th/cmp-cmdline",
 			"hrsh7th/cmp-emoji",
 			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-nvim-lsp-document-symbol",
 			"hrsh7th/cmp-nvim-lua",
 			"hrsh7th/cmp-path",
 			"hrsh7th/cmp-vsnip",
@@ -44,7 +43,6 @@ return {
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
-					{ name = "nvim_lsp_document_symbol" },
 					{ name = "nvim_lsp_signature_help" },
 					{ name = "nvim_lua" },
 					{ name = "buffer" },
